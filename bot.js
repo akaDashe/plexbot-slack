81aacd5296ee0fbc9dc6030b26bc51a88f943f42
Dashe <dashe@intercom.io>
2016-12-28 16:36:52 +0000

    Switching Framework.

--- bot.js
+++ bot.js
@@ -1,54 +1,44 @@
+//===
+//SLACK dummy bot for botkit
+//===
+
+//test environment
+if (!process.env.SLACK_TOKEN) {
+    console.log('Error: Specify SLACK_TOKEN in environment');
+    process.exit(1);
+}
+
+//get BotKit to spawn bot
 var Botkit = require('botkit');
-var redis = require('botkit/lib/storage/redis_storage');
-var http = require('http');
-var url = require('url');
-
-var redisURL = url.parse(process.env.REDISCLOUD_URL);
-var redisStorage = redis({
-    namespace: 'botkit-example',
-    host: redisURL.hostname,
-    port: redisURL.port,
-    auth_pass: redisURL.auth.split(":")[1]
-});
-
 var controller = Botkit.slackbot({
-    storage: redisStorage
+ debug: false
 });
-
 var bot = controller.spawn({
-    token: process.env.SLACK_TOKEN
-}).startRTM();
+  token: process.env.SLACK_TOKEN
+});
 
-controller.hears(['call me (.*)'],'direct_message,direct_mention,mention',function(bot, message) {
-    var matches = message.text.match(/call me (.*)/i);
-    var name = matches[1];
-    controller.storage.users.get(message.user,function(err, user) {
-        if (!user) {
-            user = {
-                id: message.user,
-            };
-        }
-        user.name = name;
-        controller.storage.users.save(user,function(err, id) {
-            bot.reply(message,'Got it. I will call you ' + user.name + ' from now on.');
-        });
+// start Slack RTM
+bot.startRTM(function(err,bot,payload) {
+  // handle errors...
+});
+
+//prepare the webhook
+controller.setupWebserver(process.env.PORT || 3001, function(err, webserver) {
+    controller.createWebhookEndpoints(webserver, bot, function() {
+        // handle errors...
     });
 });
 
-controller.hears(['what is my name','who am i'],'direct_message,direct_mention,mention',function(bot, message) {
+//===
+//bot commands
+//===
 
-    controller.storage.users.get(message.user,function(err, user) {
-        if (user && user.name) {
-            bot.reply(message,'Your name is ' + user.name);
-        } else {
-            bot.reply(message,'I don\'t know yet!');
-        }
-    });
+controller.hears(['hello','hi'],['direct_message','direct_mention','mention'],function(bot,message) {
+    bot.reply(message,"Hello.");
 });
 
-// To keep Heroku's free dyno awake
-http.createServer(function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Ok, dyno is awake.');
-}).listen(process.env.PORT || 5000);
 
+controller.on('slash_command',function(bot,message) {
+  // reply to slash command
+  bot.replyPublic(message,'Everyone can see the results of this slash command');
+});
